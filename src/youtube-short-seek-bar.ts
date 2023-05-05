0466e141d58d27f11e9e57c298662bdfb6f0ae35
PG-Refactor <naoya.h11.0508@gmail.com>
2023-05-05 13:50:16 +0000

    Create ver1.0.0

--- src/youtube-short-seek-bar.ts
+++ src/youtube-short-seek-bar.ts
@@ -1,19 +1,22 @@
 (() => {
     class ProgressBar {
-        private video: HTMLVideoElement | null;
+        private video: HTMLMediaElement | null;
         private ytIconProgressBars: HTMLElement[];
         private progressBars: HTMLProgressElement[];
+        private currentProgressBar: HTMLProgressElement | null;
+
 
         constructor(rootElementName: string) {
             this.video = this.getVideo();
             [this.ytIconProgressBars, this.progressBars] = this.create(rootElementName);
+            this.currentProgressBar = null;
         }
 
-        getVideo() {
-            const videos = document.querySelectorAll("video");
+        private getVideo = () => {
+            const videos = document.querySelectorAll('video');
             let video = null;
             for (let i = 0; i < videos.length; i++) {
-                if (videos[i].hasAttribute("src")) {
+                if (videos[i].hasAttribute('src')) {
                     video = videos[i];
                     break;
                 }
@@ -22,19 +25,22 @@
             return video;
         }
 
-        create(rootElementName: string): [HTMLElement[], HTMLProgressElement[]] {
+        private create = (rootElementName: string): [HTMLElement[], HTMLProgressElement[]] => {
             const roots = document.querySelectorAll(rootElementName);
             let progressBars: HTMLProgressElement[] = [];
             let ytIconProgressBars: HTMLElement[] = [];
             for (let i = 0; i < roots.length; i++) {
-                let progressBar = document.createElement("progress");
-                progressBar.max = this.video?.duration!;
+                let progressBar = document.createElement('progress');
+                if (!isNaN(this.video?.duration!))
+                    progressBar.max = this.video?.duration!;
                 progressBar.value = 0;
-                progressBar.classList.add("youtube-short-extension-progress-bar");
+                progressBar.classList.add('youtube-short-extension-progress-bar');
+                // progressBar.addEventListener('click', this.progressBarClickEvent);
+                progressBar.addEventListener('mousedown', this.progressBarClickEvent);
                 progressBars.push(progressBar);
-                let ytIconProgressBar = document.createElement("yt-icon-progress-bar");
-                ytIconProgressBar.classList.add("style-scope");
-                ytIconProgressBar.classList.add("ytd-shorts-player-controls");
+                let ytIconProgressBar = document.createElement('yt-icon-progress-bar');
+                ytIconProgressBar.classList.add('style-scope');
+                ytIconProgressBar.classList.add('ytd-shorts-player-controls"');
                 ytIconProgressBars.push(ytIconProgressBar);
                 ytIconProgressBar.appendChild(progressBar);
                 roots[i].appendChild(ytIconProgressBar);
@@ -43,20 +49,48 @@
             return [ytIconProgressBars, progressBars];
         }
 
-        remove() {
+        private updateVideoTime = (time: number) => {
+            if (this.video !== null)
+                this.video.currentTime = time;
+        }
+
+        private progressBarClickEvent = (e: MouseEvent) => {
+            e.preventDefault();
+            this.currentProgressBar = e.currentTarget as HTMLProgressElement;
+            document.addEventListener('mousemove', this.updateVideoTimeAndProgressBar);
+            document.addEventListener('mouseup', (e: MouseEvent) => {
+                e.preventDefault();
+                document.removeEventListener('mousemove', this.updateVideoTimeAndProgressBar);
+                this.currentProgressBar?.addEventListener('mousedown', this.progressBarClickEvent);
+            }, { once: true });
+        };
+
+        private updateVideoTimeAndProgressBar = (e: MouseEvent) => {
+            if (this.video !== null) {
+                e.preventDefault();
+                let ratio = (e.clientX - this.currentProgressBar?.getBoundingClientRect().x!) / this.currentProgressBar?.clientWidth!;
+                if (ratio < 0)
+                    ratio = 0;
+                else if (ratio > 1)
+                    ratio = 1;
+
+                const value = this.video.duration * ratio;
+                this.updateVideoTime(value);
+                this.updateProgressBar();
+            }
+        };
+        
+        public remove = () => {
             for (let i = 0; i < this.ytIconProgressBars.length; i++)
                 this.ytIconProgressBars[i].remove();
         }
 
-        updateProgressBar() {
-            let value = this.video?.currentTime!;
-            for (let i = 0; i < this.progressBars.length; i++)
-                this.progressBars[i].value = value;
-        }
-
-        updateVideoTime() {
-            if (this.video !== null)
-                this.video.currentTime = this.progressBars[0].value;
+        public updateProgressBar = () => {
+            const value = this.video?.currentTime!;
+            if (!isNaN(value)) {
+                for (let i = 0; i < this.progressBars.length; i++)
+                    this.progressBars[i].value = value;
+            }
         }
     };
     
@@ -81,7 +115,7 @@
     const main = () => {
         const url = location.href;
         if (location.href.indexOf('https://www.youtube.com/shorts') === 0) {
-            const rootElementName = "ytd-shorts-player-controls";
+            const rootElementName = 'ytd-shorts-player-controls';
             const progressBar = new ProgressBar(rootElementName);
             
             const interval = setInterval(() => {
