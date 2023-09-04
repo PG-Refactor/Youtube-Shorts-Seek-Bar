dd6e3ab193c3795c0073626fd3644c57e14831f3
PG-Refactor <naoya.h11.0508@gmail.com>
2023-09-04 18:08:13 +0000

    Add Volume bar.

--- src/youtube-shorts-seek-bar.ts
+++ src/youtube-shorts-seek-bar.ts
@@ -93,6 +93,100 @@
             }
         }
     };
+
+    class VolumeBar {
+        private video: HTMLMediaElement | null;
+        private ytIconVolumeBars: HTMLElement[];
+        private volumeBars: HTMLProgressElement[];
+        private currentVolumeBar: HTMLProgressElement | null;
+
+
+        constructor(rootElementName: string) {
+            this.video = this.getVideo();
+            [this.ytIconVolumeBars, this.volumeBars] = this.create(rootElementName);
+            this.currentVolumeBar = null;
+        }
+
+        private getVideo = () => {
+            const videos = document.querySelectorAll('video');
+            let video: HTMLMediaElement | null  = null;
+            for (let i = 0; i < videos.length; i++) {
+                if (videos[i].hasAttribute('src')) {
+                    video = videos[i];
+                    break;
+                }
+            }
+
+            return video;
+        }
+
+        private create = (rootElementName: string): [HTMLElement[], HTMLProgressElement[]] => {
+            const roots = document.querySelectorAll(rootElementName);
+            let volumeBars: HTMLProgressElement[] = [];
+            let ytIconVolumeBars: HTMLElement[] = [];
+            for (let i = 0; i < roots.length; i++) {
+                let volumeBar = document.createElement('progress');
+                volumeBar.max = 1;
+                volumeBar.value = this.video?.volume!;
+                volumeBar.classList.add('youtube-shorts-extension-volume-bar');
+                volumeBar.addEventListener('mousedown', this.volumeBarClickEvent);
+                volumeBars.push(volumeBar);
+                let ytIconVolumeBar = document.createElement('yt-icon-volume-bar');
+                ytIconVolumeBar.classList.add('style-scope');
+                ytIconVolumeBar.classList.add('ytd-shorts-player-controls"');
+                ytIconVolumeBars.push(ytIconVolumeBar);
+                ytIconVolumeBar.appendChild(volumeBar);
+                roots[i].appendChild(ytIconVolumeBar);
+            }
+
+            return [ytIconVolumeBars, volumeBars];
+        }
+
+        private updateVideoVolume = (volume: number) => {
+            if (this.video !== null)
+                this.video.volume = volume;
+        }
+
+        private volumeBarClickEvent = (e: MouseEvent) => {
+            e.preventDefault();
+            this.currentVolumeBar = e.currentTarget as HTMLProgressElement;
+            this.updateVideoVolumeAndVolumeBar(e);
+            document.addEventListener('mousemove', this.updateVideoVolumeAndVolumeBar);
+            document.addEventListener('mouseup', (e: MouseEvent) => {
+                e.preventDefault();
+                document.removeEventListener('mousemove', this.updateVideoVolumeAndVolumeBar);
+                this.currentVolumeBar?.addEventListener('mousedown', this.volumeBarClickEvent);
+            }, { once: true });
+        };
+
+        private updateVideoVolumeAndVolumeBar = (e: MouseEvent) => {
+            if (this.video !== null) {
+                e.preventDefault();
+                let ratio = (e.clientX - this.currentVolumeBar?.getBoundingClientRect().x!) / this.currentVolumeBar?.clientWidth!;
+                if (ratio < 0)
+                    ratio = 0;
+                else if (ratio > 1)
+                    ratio = 1;
+
+                const value = ratio;
+                this.updateVideoVolume(value);
+                this.updateVolumeBar();
+            }
+        };
+        
+        public remove = () => {
+            for (let i = 0; i < this.ytIconVolumeBars.length; i++)
+                this.ytIconVolumeBars[i].remove();
+        }
+
+        public updateVolumeBar = () => {
+            const value = this.video?.volume!;
+            if (!isNaN(value)) {
+                for (let i = 0; i < this.volumeBars.length; i++)
+                    this.volumeBars[i].value = value;
+            }
+        }
+    };
     
     const sleep = (second: number, callbackFunc: (arg?: any) => void, arg: any = null) => {
         let elapsedTime = 0;
@@ -116,12 +210,14 @@
         const url = location.href;
         if (location.href.indexOf('https://www.youtube.com/shorts') === 0) {
             const rootElementName = 'ytd-shorts-player-controls';
+            const volumeBar = new VolumeBar(rootElementName);
             const progressBar = new ProgressBar(rootElementName);
             
             const interval = setInterval(() => {
                 progressBar.updateProgressBar();
                 if (url !== location.href) {
                     clearInterval(interval);
+                    volumeBar.remove();
                     progressBar.remove();
                     if (document.visibilityState === 'visible')
                         sleep(1, main);
