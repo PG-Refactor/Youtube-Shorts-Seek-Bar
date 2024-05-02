fc6bf7f384ab6f85f0ed3a2d569133718663c48d
PG-Refactor <naoya.h11.0508@gmail.com>
2024-05-02 17:42:39 +0000

    Update styles.

--- src/youtube-shorts-seek-bar.ts
+++ src/youtube-shorts-seek-bar.ts
@@ -1,15 +1,13 @@
 (() => {
     class ProgressBar {
         private video: HTMLMediaElement | null;
-        private ytIconProgressBars: HTMLElement[];
-        private progressBars: HTMLProgressElement[];
-        private currentProgressBar: HTMLProgressElement | null;
+        private ytIconProgressBar: HTMLElement;
+        private progressBar: HTMLProgressElement;
 
 
         constructor(rootElementName: string) {
             this.video = this.getVideo();
-            [this.ytIconProgressBars, this.progressBars] = this.create(rootElementName);
-            this.currentProgressBar = null;
+            [this.ytIconProgressBar, this.progressBar] = this.create(rootElementName);
         }
 
         private getVideo = () => {
@@ -25,27 +23,21 @@
             return video;
         }
 
-        private create = (rootElementName: string): [HTMLElement[], HTMLProgressElement[]] => {
-            const roots = document.querySelectorAll(rootElementName);
-            let progressBars: HTMLProgressElement[] = [];
-            let ytIconProgressBars: HTMLElement[] = [];
-            for (let i = 0; i < roots.length; i++) {
-                let progressBar = document.createElement('progress');
-                if (!isNaN(this.video?.duration!))
-                    progressBar.max = this.video?.duration!;
-                progressBar.value = 0;
-                progressBar.classList.add('youtube-shorts-extension-progress-bar');
-                progressBars.push(progressBar);
-                let ytIconProgressBar = document.createElement('yt-icon-progress-bar');
-                ytIconProgressBar.classList.add('style-scope');
-                ytIconProgressBar.classList.add('ytd-shorts-player-controls"');
-                ytIconProgressBar.addEventListener('mousedown', this.progressBarClickEvent);
-                ytIconProgressBars.push(ytIconProgressBar);
-                ytIconProgressBar.appendChild(progressBar);
-                roots[i].appendChild(ytIconProgressBar);
-            }
+        private create = (beforeElementName: string): [HTMLElement, HTMLProgressElement] => {
+            const beforeElement = document.querySelector(beforeElementName);
+            let progressBar = document.createElement('progress');
+            if (!isNaN(this.video?.duration!))
+                progressBar.max = this.video?.duration!;
+            progressBar.value = 0;
+            progressBar.classList.add('youtube-shorts-extension-progress-bar');
+            let ytIconProgressBar = document.createElement('yt-icon-progress-bar');
+            ytIconProgressBar.classList.add('style-scope');
+            ytIconProgressBar.classList.add('ytd-shorts-player-controls"');
+            ytIconProgressBar.addEventListener('mousedown', this.progressBarClickEvent);
+            ytIconProgressBar.appendChild(progressBar);
+            beforeElement?.after(ytIconProgressBar);
 
-            return [ytIconProgressBars, progressBars];
+            return [ytIconProgressBar, progressBar];
         }
 
         private updateVideoTime = (time: number) => {
@@ -56,7 +48,6 @@
         private progressBarClickEvent = (e: MouseEvent) => {
             e.preventDefault();
             const ytIconProgressBar = e.currentTarget as HTMLElement;
-            this.currentProgressBar = ytIconProgressBar?.querySelector('.youtube-shorts-extension-progress-bar');
             this.updateVideoTimeAndProgressBar(e);
             document.addEventListener('mousemove', this.updateVideoTimeAndProgressBar);
             document.addEventListener('mouseup', (e: MouseEvent) => {
@@ -69,7 +60,7 @@
         private updateVideoTimeAndProgressBar = (e: MouseEvent) => {
             if (this.video !== null) {
                 e.preventDefault();
-                let ratio = (e.clientX - this.currentProgressBar?.getBoundingClientRect().x!) / this.currentProgressBar?.clientWidth!;
+                let ratio = (e.clientX - this.progressBar?.getBoundingClientRect().x!) / this.progressBar?.clientWidth!;
                 if (ratio < 0)
                     ratio = 0;
                 else if (ratio > 1)
@@ -80,113 +71,11 @@
                 this.updateProgressBar();
             }
         };
-        
-        public remove = () => {
-            for (let i = 0; i < this.ytIconProgressBars.length; i++)
-                this.ytIconProgressBars[i].remove();
-        }
 
         public updateProgressBar = () => {
             const value = this.video?.currentTime!;
-            if (!isNaN(value)) {
-                for (let i = 0; i < this.progressBars.length; i++)
-                    this.progressBars[i].value = value;
-            }
-        }
-    };
-
-    class VolumeBar {
-        private video: HTMLMediaElement | null;
-        private ytIconVolumeBars: HTMLElement[];
-        private volumeBars: HTMLProgressElement[];
-        private currentVolumeBar: HTMLProgressElement | null;
-
-
-        constructor(rootElementName: string) {
-            this.video = this.getVideo();
-            [this.ytIconVolumeBars, this.volumeBars] = this.create(rootElementName);
-            this.currentVolumeBar = null;
-        }
-
-        private getVideo = () => {
-            const videos = document.querySelectorAll('video');
-            let video: HTMLMediaElement | null  = null;
-            for (let i = 0; i < videos.length; i++) {
-                if (videos[i].hasAttribute('src')) {
-                    video = videos[i];
-                    break;
-                }
-            }
-
-            return video;
-        }
-
-        private create = (rootElementName: string): [HTMLElement[], HTMLProgressElement[]] => {
-            const roots = document.querySelectorAll(rootElementName);
-            let volumeBars: HTMLProgressElement[] = [];
-            let ytIconVolumeBars: HTMLElement[] = [];
-            for (let i = 0; i < roots.length; i++) {
-                let volumeBar = document.createElement('progress');
-                volumeBar.max = 1;
-                volumeBar.value = this.video?.volume!;
-                volumeBar.classList.add('youtube-shorts-extension-volume-bar');
-                volumeBars.push(volumeBar);
-                let ytIconVolumeBar = document.createElement('yt-icon-volume-bar');
-                ytIconVolumeBar.classList.add('style-scope');
-                ytIconVolumeBar.classList.add('ytd-shorts-player-controls"');
-                ytIconVolumeBar.addEventListener('mousedown', this.volumeBarClickEvent);
-                ytIconVolumeBars.push(ytIconVolumeBar);
-                ytIconVolumeBar.appendChild(volumeBar);
-                roots[i].appendChild(ytIconVolumeBar);
-            }
-
-            return [ytIconVolumeBars, volumeBars];
-        }
-
-        private updateVideoVolume = (volume: number) => {
-            if (this.video !== null)
-                this.video.volume = volume;
-        }
-
-        private volumeBarClickEvent = (e: MouseEvent) => {
-            e.preventDefault();
-            const ytIconVolumeBar = e.currentTarget as HTMLElement
-            this.currentVolumeBar = ytIconVolumeBar?.querySelector('.youtube-shorts-extension-volume-bar');
-            this.updateVideoVolumeAndVolumeBar(e);
-            document.addEventListener('mousemove', this.updateVideoVolumeAndVolumeBar);
-            document.addEventListener('mouseup', (e: MouseEvent) => {
-                e.preventDefault();
-                document.removeEventListener('mousemove', this.updateVideoVolumeAndVolumeBar);
-                ytIconVolumeBar?.addEventListener('mousedown', this.volumeBarClickEvent);
-            }, { once: true });
-        };
-
-        private updateVideoVolumeAndVolumeBar = (e: MouseEvent) => {
-            if (this.video !== null) {
-                e.preventDefault();
-                let ratio = (e.clientX - this.currentVolumeBar?.getBoundingClientRect().x!) / this.currentVolumeBar?.clientWidth!;
-                if (ratio < 0)
-                    ratio = 0;
-                else if (ratio > 1)
-                    ratio = 1;
-
-                const value = ratio;
-                this.updateVideoVolume(value);
-                this.updateVolumeBar();
-            }
-        };
-        
-        public remove = () => {
-            for (let i = 0; i < this.ytIconVolumeBars.length; i++)
-                this.ytIconVolumeBars[i].remove();
-        }
-
-        public updateVolumeBar = () => {
-            const value = this.video?.volume!;
-            if (!isNaN(value)) {
-                for (let i = 0; i < this.volumeBars.length; i++)
-                    this.volumeBars[i].value = value;
-            }
+            if (!isNaN(value))
+                this.progressBar.value = value;
         }
     };
     
@@ -210,17 +99,25 @@
 
     const main = () => {
         const url = location.href;
-        if (location.href.indexOf('https://www.youtube.com/shorts') === 0) {
-            const rootElementName = 'ytd-shorts-player-controls';
-            const volumeBar = new VolumeBar(rootElementName);
-            const progressBar = new ProgressBar(rootElementName);
+        
+        if (url.indexOf('https://www.youtube.com/shorts') === 0) {
+            const beforeElementName = '#play-pause-button-shape';
+            const progressBar = new ProgressBar(beforeElementName);
             
             const interval = setInterval(() => {
                 progressBar.updateProgressBar();
-                if (url !== location.href) {
+                if (location.href.indexOf('https://www.youtube.com/shorts') !== 0) {
                     clearInterval(interval);
-                    volumeBar.remove();
-                    progressBar.remove();
+                    if (document.visibilityState === 'visible')
+                        sleep(1, main);
+                    else {
+                        window.addEventListener('focus', () => {
+                            sleep(1, main);
+                        }, {once: true});
+                    }
+                }
+                else if (document.querySelector('yt-icon-progress-bar') === null) {
+                    clearInterval(interval);
                     if (document.visibilityState === 'visible')
                         sleep(1, main);
                     else {
